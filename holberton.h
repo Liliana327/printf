c5c970772dce918a0bfa27ddac873e6f313481ea
Liliana327 <894@holbertonschool.com>
2019-07-28 14:18:50 +0000

    Added library with prototypes

---
+++ holberton.h
@@ -0,0 +1,8 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
+int _printf(const char *format, ...);
+char *convert_to(unsigned int, int);
+#endif /** _HOLBERTON_H */ 
